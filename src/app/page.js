9552aa31a099b638f492ddb2b774c066d5848da6
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2024-04-02 00:32:14 +0900

    before external image cover https://youtu.be/vCOSTG10Y4o

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
+import Image from "next/image"
+import "./globals.css"
+
 export default function Home() {
-  return <div className={`styles.container`}>부산 깡통시장 구매대행</div>
+  return (
+    <>
+      <div className={`styles.container`}>부산 깡통시장 구매대행</div>
+      <div className="imageContainer">
+        <Image src="/orihiro.jpg" alt="" fill></Image>
+      </div>
+    </>
+  )
 }
--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -2,8 +2,11 @@
 import Link from "next/link" // 주의: 이 코드 예제에서는 Link 컴포넌트가 사용되지 않고 있습니다.
 import styles from "./links.module.css"
 import NavLink from "./navLink/navLink"
+import {useState} from "react"
+import Image from "next/image"
 
 const Links = () => {
+  const [open, setOpen] = useState(false)
   const links = [
     {title: "home", path: "/"},
     {title: "order", path: "/order"}
@@ -12,24 +15,41 @@ const Links = () => {
   const isAdmin = true
 
   return (
-    <div className={styles.links}>
-      {links.map(link => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}} />}
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{title: "login", path: "/login"}} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map(link => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session?.user ? (
+          <>
+            {session.user?.isAdmin && (
+              <NavLink item={{title: "Admin", path: "/admin"}} />
+            )}
+            <form action={handleLogout}>
+              <button className={styles.logout}>Logout</button>
+            </form>
+          </>
+        ) : (
+          <NavLink item={{title: "Login", path: "/login"}} />
+        )}
+      </div>
+      <Image
+        className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen(prev => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map(link => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   )
-
-  function handleLogout() {
-    // 로그아웃 처리 로직 구현
-  }
 }
 
 export default Links
