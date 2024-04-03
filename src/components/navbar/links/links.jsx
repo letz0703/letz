7fc49acbf88e883b437f70b6dd94b58307c84170
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2024-04-04 00:49:01 +0900

    fix eslint

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -1,5 +1,4 @@
 "use client"
-import Link from "next/link" // 주의: 이 코드 예제에서는 Link 컴포넌트가 사용되지 않고 있습니다.
 import styles from "./links.module.css"
 import NavLink from "./navLink/navLink"
 import {useState} from "react"
@@ -12,11 +11,10 @@ const Links = () => {
     {title: "order", path: "/order"}
   ]
   const session = true
-  const isAdmin = true
 
   return (
     <div className={styles.container}>
-      <div className={styles.links}>
+      {/*<div className={styles.links}>
         {links.map(link => (
           <NavLink item={link} key={link.title} />
         ))}
@@ -47,7 +45,7 @@ const Links = () => {
             <NavLink item={link} key={link.title} />
           ))}
         </div>
-      )}
+      )}*/}
     </div>
   )
 }
