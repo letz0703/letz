960bb681a719983fa8e6de2e82af293150f39d6b
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2024-04-04 00:36:41 +0900

    배경에 버튼 달기

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
 import "./globals.css"
 
 export default function Home() {
-  return <div className="container flex text-center background"></div>
+  return (
+    <div className="container flex text-center background">
+      {/*<Image src="/main.jpg" layout="fill" objectFit="cover" alt="background" />*/}
+      <div className="linkContainer">
+        <a
+          href="https://www.pinterest.co.kr/canmart051/"
+          className="linkButton" // 클래스 이름이 linkBuggon에서 linkButton으로 오타 수정
+          rel="noopener noreferrer" // 보안 관련 속성 추가
+          target="_blank" // 새 탭에서 링크 열기
+        >
+          아이템 보기
+        </a>
+      </div>
+    </div>
+  )
 }
