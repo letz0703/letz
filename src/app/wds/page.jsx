4120f5e1475a726d583c1fb99fbcebc945a0a798
Chang Man Kim <22319355+letz0703@users.noreply.github.com>
2024-06-24 21:38:43 +0900

    검색 달기전 2024.06.24 월

--- src/app/wds/page.jsx
+++ src/app/wds/page.jsx
@@ -1,18 +1,43 @@
 'use client'
 import {use, useEffect, useState} from "react"
 
-//import styles from './page.module.css'
+import styles from './wds.css'
 const Page = () => {
-const [stateName, setStateName] = useState("LETz iCANMART")
+const [title, settitle] = useState("letz");
+const [product, setProduct] = useState();
+const [setLoading, setSetLoading] = useState(true);
 
 useEffect(() => {
-	document.title = stateName
-}, [stateName]);
+	const handler = () => {
+		document.title = title +" info"
+		console.log(title);
+	}
 
+	const timeout = setTiemout(() => {
+		console.log(`this page name is ${title}`)
+	},1000)
+
+	document.addEventListener("click", handler)
+	return () => {
+		//document.removeEventListener("click", handler)
+		clearTimeout(timeout)
+	}
+}, [title]);
+
+useEffect(() => {
+	fetch('').then(res => res.JSON()).then(data=> {
+		setProduct(data)
+	}).finally(() => {
+		setLoading(false)
+	})
+}, []);
 
 	return (
 		<div>
-			<h2>{stateName}</h2>
+			<input value={title} onChange={e => settitle(e.target.value)}/>
+			<button className="btn_ic" onClick={() => settitle(title)}>button</button>
+			<h1>Page Title is {title} 검색</h1>
+			<h1>{JSON.stringify(product)}</h1>
 		</div>
 	)
 }
